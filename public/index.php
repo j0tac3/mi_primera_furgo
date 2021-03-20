c60e8d177b3752d8fbc2a14e8cd2aa9b2ba4b1ee
unknown <j0s3.90jr@gmail.com>
2021-03-20 14:25:03 +0100

    config cors

--- public/index.php
+++ public/index.php
@@ -4,7 +4,11 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+    header('Access-Control-Allow-Origin: *'); 
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 /*
 |--------------------------------------------------------------------------
 | Check If Application Is Under Maintenance
