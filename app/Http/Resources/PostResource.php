cfe9ead349324ced21300757eca1c67807734ca1
unknown <j0s3.90jr@gmail.com>
2021-04-15 00:43:29 +0200

    create Resourcer Post

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -24,7 +24,7 @@ class PostResource extends JsonResource
             'user_id' => $this->user_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'secciones' => SeccionResource::collection($this->whenLoaded('secciones')),
+            'secciones' => SeccionResource::collection($this->secciones),
         ];
     }
 
