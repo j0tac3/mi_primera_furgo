754f79195f1d74e54a772c8798400ab048310166
unknown <j0s3.90jr@gmail.com>
2021-04-14 23:34:06 +0200

    create Resourcer Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        $etiquetas = Post::all();
-        return $etiquetas;
+        $post = Post::paginate(10);
+        return PostResource::collection($post);
     }
 
     public function show($id) {
         $post = Post::findOrFail($id);
-        return $post;
+        return PostResource::collection($post);
     }
 
     public function store(Request $request) {
@@ -23,8 +24,15 @@ class PostController extends Controller
             //'image_url' => 'required',
             //'user_id' => 'required'
         ]);
-            
-        return Post::create($request->all());
+        //return Post::create($request->all())
+
+        $post = new Post();
+        $post->titulo = $request->titulo;
+        $post->image_url = $request->image_url;
+        $post->user_id = $request->user_id;
+        if ($post->save()){
+            return new PostResource($post);
+        }
     }
 
     public function update(Request $request, $id) {
@@ -35,11 +43,20 @@ class PostController extends Controller
         ]);
         
         $post = Post::findOrFail($id);       
-        return  $post->update($request->all());
+        //return  $post->update($request->all());
+        $post->titulo = $request->titulo;
+        $post->image_url = $request->image_url;
+        $post->user_id = $request->user_id;
+        if ($post->save()){
+            return new PostResource($post);
+        }
     }
 
     public function destroy($id) {
         $post = Post::findOrFail($id);        
-        return $post->delete();
+        //return $post->delete();
+        if ($post->delete()){
+            return new PostResource($post);
+        }
     }
 }
