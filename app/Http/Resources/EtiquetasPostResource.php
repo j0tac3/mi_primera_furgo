a77624913fd73dda3acc821152690b436684d5d5
unknown <j0s3.90jr@gmail.com>
2021-04-16 21:58:53 +0200

    create Resourcer Post

--- app/Http/Resources/EtiquetasPostResource.php
+++ app/Http/Resources/EtiquetasPostResource.php
@@ -17,8 +17,8 @@ class EtiquetasPostResource extends JsonResource
         ///return parent::toArray($request);
         return [
             'id' => $this->id,
-            'post_id' => $this->id_post,
-            'etiqueta_id' => $this->id_etiqueta,
+            'post_id' => $this->post_id,
+            'etiqueta_id' => $this->etiqueta_id,
             'post' => $this->post,
         ];
     }
