3ec1472b3d5963dcaeb9d47ccf52c4bd57eac9ab
unknown <j0s3.90jr@gmail.com>
2021-04-21 23:24:04 +0200

    create Resourcer Post

--- app/Models/EtiquetasPost.php
+++ app/Models/EtiquetasPost.php
@@ -11,10 +11,10 @@ class EtiquetasPost extends Model
     protected $fillable = array('id', 'etiqueta_id', 'post_id');
 
     public function post(){
-        return $this->belongsTo(Post::class, 'post_id', 'id')->withDefault();
+        return $this->hasOne(Post::class, 'post_id', 'id')->withDefault();
     }
 
     public function etiqueta(){
-        return $this->belongsTo(Etiqueta::class, 'etiqueta_id', 'id')->withDefault();
+        return $this->hasOne(Etiqueta::class, 'etiqueta_id', 'id')->withDefault();
     }
 }
