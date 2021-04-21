8145c20076a1f717a87ec45971744bc504cfb8f9
unknown <j0s3.90jr@gmail.com>
2021-04-21 23:22:03 +0200

    create Resourcer Post

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -26,7 +26,7 @@ class PostResource extends JsonResource
             'user_id' => $this->user_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'etiquetas' => (new EtiquetasPostResource($etiquetasPost)),
+            'etiquetas' => EtiquetasPostResource::collection(($this->etiquetaPost)),
         ];
     }
 
