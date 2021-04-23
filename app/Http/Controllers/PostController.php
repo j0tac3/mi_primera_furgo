f707f9980728e714c038c0ccdf10678668f87788
unknown <j0s3.90jr@gmail.com>
2021-04-23 20:38:26 +0200

    create Resourcer Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,14 +35,14 @@ class PostController extends Controller
         }
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request) {
         $request->validate([
             'titulo' => 'required',
             //'image_url' => 'required',
             //'user_id' => 'required'
         ]);
         
-        $post = Post::findOrFail($id);       
+        $post = Post::findOrFail($request->id);       
         //return  $post->update($request->all());
         $post->titulo = $request->titulo;
         $post->image_url = $request->image_url;
