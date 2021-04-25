141842e841866b4254127fac85505194719f7571
unknown <j0s3.90jr@gmail.com>
2021-04-25 10:07:16 +0200

    create Resourcer Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -54,10 +54,10 @@ class PostController extends Controller
         $post->subtitulo = $request->subtitulo;
         $post->user_id = $request->user_id;
 
-        $path = Storage::putFile('photos', $request->file('headerImage'));
+        $path = Storage::putFile('images', $request->file('headerImage'));
         //$post->image_url = $request->image_url;
         $post->image_url = $path;
-        
+
         if ($post->save()){
             return new PostResource($post);
         }
