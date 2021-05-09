bee92a3c829fcd569a4a32459454d800039e2d90
unknown <j0s3.90jr@gmail.com>
2021-05-10 00:38:08 +0200

    Añadido el Resource de Comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index() {
         $post = Post::latest()->paginate(10);
-        return PostResource::collection($post)->take(2);
+        return PostResource::collection($post);
     }
 
     public function show($id) {
