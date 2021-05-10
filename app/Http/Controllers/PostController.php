016d03d3fae55041f19df6b45e532566b0d27ce3
unknown <j0s3.90jr@gmail.com>
2021-05-10 19:30:54 +0200

    Añadido el Resource de Comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -89,7 +89,7 @@ class PostController extends Controller
     }
 
     public function getMostRecentPosts(){
-        $mostRecentPosts = Post::take(-3)->get();
+        $mostRecentPosts = Post::take(2)->get();
         return PostResource::collection($mostRecentPosts);
     }
 }
