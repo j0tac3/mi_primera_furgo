14c11f67c0a8dd577e1d6db09884ffcdf48ad971
unknown <j0s3.90jr@gmail.com>
2021-05-14 11:17:22 +0200

    Añadido el Resource de Comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,9 +36,11 @@ class PostController extends Controller
         if ($request->file('headerImage')){
             //dd($request->file('headerImage'));
             //dd($request->headerImage);
-            $imageName =  public_path('/images').$request->file('headerImage')->getClientOriginalName();
+            $imageName =  public_path('/images');
+            $imageFinalURL = $imageName.'/'.$request->file('headerImage')->getClientOriginalName();
             dd($imageName);
             $image_path = Storage::disk('local')->put($imageName, File::get($request->file('headerImage')));
+            dd($image_path);
             $post->image_url = $imageName;
         }
        /*  if (Post::create($request->all())){
