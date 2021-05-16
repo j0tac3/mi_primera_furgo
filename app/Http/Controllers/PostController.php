bca15410bb90fcad1024bfdfadc6524048d18c3a
unknown <j0s3.90jr@gmail.com>
2021-05-16 11:45:45 +0200

    Añadido el Resource de Comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,7 +38,9 @@ class PostController extends Controller
             $imageFinalURL = $imageName.'/'.$request->file('headerImage')->getClientOriginalName();
             //$image_path = Storage::disk('local')->put($imageFinalURL, File::get($request->file('headerImage')));
             $imageFile = $request->file('headerImage')->storeAs('images',$request->file('headerImage')->getClientOriginalName());
+            dd($imageFile);
             $post->image_url = $request->file('headerImage')->getClientOriginalName();
+            dd($post->image_url);
         }
        /*  if (Post::create($request->all())){
             return new PostResource($request);
