f8b74082d0ef0bb2bcff7a1c2e5c5a7ac1a33ec4
unknown <j0s3.90jr@gmail.com>
2021-06-29 10:28:11 +0200

    Creado los elementos de Aventruas y ElementsAventura

--- app/Http/Controllers/ElementsaventuraController.php
+++ app/Http/Controllers/ElementsaventuraController.php
@@ -40,7 +40,7 @@ class ElementsaventuraController extends Controller
     public function store(Request $request)
     {
         $elements = $request->all();
-        dd($elements[0]->element);
+        dd($request->toJson());
         foreach ($request->all() as $element) {
             dd($element);
             $elementsAventura = new Elementsaventura();
